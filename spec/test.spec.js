c57f3c98b09f2237e2f705e8af33d9a9bcad305a
Kuma Li <li.jun.kuma@gmail.com>
2019-02-19 10:31:02 +0800

    fix: change test function order

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -4,6 +4,22 @@ let filesAdapterTests = require('parse-server-conformance-tests').files;
 let COSAdapter = require('../index.js');
 
 describe('COSAdapter tests', () => {
+  if (
+    process.env.COS_SECRET_ID
+    && process.env.COS_SECRET_KEY
+    && process.env.COS_BUCKET
+    && process.env.COS_REGION
+  ) {
+    // Should be initialized from the env
+    let cosAdapter = new COSAdapter();
+    filesAdapterTests.testAdapter("COSAdapter", cosAdapter);
+  }
+
+  delete process.env.COS_SECRET_ID;
+  delete process.env.COS_SECRET_KEY;
+  delete process.env.COS_BUCKET;
+  delete process.env.COS_REGION;
+
   it('should throw when not initialized properly', () => {
     expect(() => {
       return new COSAdapter('');
@@ -43,16 +59,4 @@ describe('COSAdapter tests', () => {
       return new COSAdapter({ SecretId: 'SecretId' , SecretKey: 'SecretKey', Region: 'Region', Bucket: 'Bucket'});
     }).not.toThrow();
   });
-
-  if (
-    process.env.COS_SECRET_ID
-    && process.env.COS_SECRET_KEY
-    && process.env.COS_BUCKET
-    && process.env.COS_REGION
-  ) {
-    // Should be initialized from the env
-    let cosAdapter = new COSAdapter();
-    filesAdapterTests.testAdapter("COSAdapter", cosAdapter);
-  }
-
 });
