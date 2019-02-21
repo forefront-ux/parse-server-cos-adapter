25a6ba58bc449b7d987858a3c2994503c2675396
Kuma Li <li.jun.kuma@gmail.com>
2019-02-21 22:53:38 +0800

    fix: getFileLocation test function

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -17,21 +17,37 @@ describe('COSAdapter tests', () => {
     filesAdapterTests.testAdapter("COSAdapter", cosAdapter);
 
     it('should not throw error of getFileLocation method', () => {
+      const SecretId = process.env.COS_SECRET_ID;
+      const SecretKey = process.env.COS_SECRET_KEY;
+      const Bucket = process.env.COS_BUCKET;
+      const Region = process.env.COS_REGION;
+      const DirectAccess = process.env.COS_DIRECT_ACCESS;
+      const Signed = process.env.COS_SIGNED;
+      const param = {
+        SecretId,
+        SecretKey,
+        Bucket,
+        Region,
+        DirectAccess,
+        Signed
+      };
+
       // test getFileLocation API
       // get signed url
+      const cosAdapterSigned = new COSAdapter(param);
       const filename = 'test.jpg';
-      let fileLocation = cosAdapter.getFileLocation({}, filename);
+      let fileLocation = cosAdapterSigned.getFileLocation({}, filename);
       const regSignedUrl = new RegExp(`^https://${process.env.COS_BUCKET}.cos.${process.env.COS_REGION}.myqcloud.com/${filename}\?.+$`);
       expect(fileLocation).toMatch(regSignedUrl);
       // get unsigned url
-      delete process.env.COS_SIGNED;
-      let cosAdapterNotSigned = new COSAdapter();
+      delete param.Signed;
+      const cosAdapterNotSigned = new COSAdapter(param);
       fileLocation = cosAdapterNotSigned.getFileLocation({}, filename);
       const strUnsignedUrl = `https://${process.env.COS_BUCKET}.cos.${process.env.COS_REGION}.myqcloud.com/${filename}`;
       expect(fileLocation).toBe(strUnsignedUrl);
       // get file path through parse-server
-      delete process.env.COS_DIRECT_ACCESS;
-      let cosAdapterNotDirectAccess = new COSAdapter();
+      delete param.DirectAccess;
+      const cosAdapterNotDirectAccess = new COSAdapter(param);
       const mount = '/assets';
       const appId = 'testApp';
       fileLocation = cosAdapterNotDirectAccess.getFileLocation({
@@ -46,6 +62,8 @@ describe('COSAdapter tests', () => {
   delete process.env.COS_SECRET_KEY;
   delete process.env.COS_BUCKET;
   delete process.env.COS_REGION;
+  delete process.env.COS_SIGNED;
+  delete process.env.COS_DIRECT_ACCESS;
 
   it('should throw when not initialized properly', () => {
     expect(() => {
